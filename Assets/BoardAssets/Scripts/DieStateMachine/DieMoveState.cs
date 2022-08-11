fdc7366017a5c5357aec91a399c96fa3ec3ae8cb
Franklin Shack <franklinshack@gmail.com>
2022-08-11 19:20:46 -0400

    Fixed the rotation bug

--- Assets/BoardAssets/Scripts/DieStateMachine/DieMoveState.cs
+++ Assets/BoardAssets/Scripts/DieStateMachine/DieMoveState.cs
@@ -5,13 +5,13 @@ public class DieMoveState : DieBaseState
 
     float _distFromCam = 15, _dieSpacing = 2;
 
-    Quaternion _rotTarget, _rotInit;
+    Quaternion  _rotInit;
 
     float _t, _time, _transitionTime = 3;
 
     Vector3 _targetPosition;
 
-    Vector3 _faceUpDir;
+    Vector3 _rotTarget, _faceUpDir;
     public override void EnterState(DieStateManager die)
     {
         SetPhysics(die);
@@ -39,34 +39,43 @@ public class DieMoveState : DieBaseState
     }
     void SetTargetRot(DieStateManager die)
     {
+        Debug.Log("Die "+die.ID+" value is: "+die.GetSideFaceUp());
         switch (die.GetSideFaceUp())
         {
             case 1:
                 _faceUpDir = -die.transform.up;
+                _rotTarget = new Vector3(90, 0, 0);
                 break;
             case 2:
                 _faceUpDir = -die.transform.forward;
+                _rotTarget = new Vector3(0, 0, 0);
                 break;
             case 3:
                 _faceUpDir = die.transform.right;
+                _rotTarget = new Vector3(0, 90, 0);
                 break;
             case 4:
                 _faceUpDir = -die.transform.right;
+                _rotTarget = new Vector3(0, 270, 0);
                 break;
             case 5:
                 _faceUpDir = die.transform.forward;
+                _rotTarget = new Vector3(180, 0, 0);
                 break;
             case 6:
                 _faceUpDir = die.transform.up;
+                _rotTarget = new Vector3(270, 0, 0);
                 break;
         }
+        Debug.Log("Die "+die.ID+"'s faceUpDir is "+_faceUpDir);
         _rotInit = die.transform.rotation;
-        _rotTarget = Quaternion.FromToRotation(_faceUpDir, -die.rollSelectCam.transform.forward);
+        Debug.Log("Die "+die.ID+"'s _rotInit is "+_rotInit);
+        Debug.Log("Die "+die.ID+" should rotate from "+_rotInit+" to "+_rotTarget);
 
     }
     void Rotate(DieStateManager die)
     {
-        die.transform.rotation = _rotTarget; //Quaternion.Slerp(_rotInit, _rotTarget, _t);
+        die.transform.eulerAngles = _rotTarget; //Quaternion.Slerp(_rotInit, _rotTarget, _t);
     }
 
     void Move(DieStateManager die)
